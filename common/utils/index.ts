2371773622dc41f4a3b2eda5221cf74b5120b642
彭道宽 <pengdaokuan@cvte.com>
2021-03-14 11:43:39 +0800

    [feature][what]添加链接处理Utils

--- common/utils/index.ts
+++ common/utils/index.ts
@@ -86,3 +86,38 @@ export async function hasFile(projectPath: string) {
     return false;
   }
 }
+
+/**
+ * @desc 判断是否属于外部连接
+ * @param {string} url - 链接
+ */
+export function isUrl(url: string): boolean {
+  let regRule = /(http|https):\/\/([\w.]+\/?)\S*/;
+  return regRule.test(url.toLowerCase());
+}
+
+/**
+ * 获取URL中的参数的值
+ * @param {string} paras 参数key
+ * @returns {string} 返回参数的value
+ */
+export function getUrlParam(paras?: string) {
+  let url = window.location.search;
+  let paramString = url.substring(url.indexOf('?') + 1, url.length).split('&');
+  let paraObj: {
+    [key: string]: string;
+  } = {};
+  let j = '';
+  for (let i = 0; (j = paramString[i]); i++) {
+    const key = j.substring(0, j.indexOf('=')).toLowerCase();
+    const value = j.substring(j.indexOf('=') + 1, j.length);
+    paraObj[key] = value;
+  }
+  let returnValue: any;
+  if (paras) {
+    returnValue = paraObj[paras.toLowerCase()];
+  } else {
+    returnValue = { ...paraObj };
+  }
+  return typeof returnValue === 'undefined' ? '' : returnValue;
+}
