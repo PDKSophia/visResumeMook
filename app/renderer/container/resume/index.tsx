8a9f917b2c67ff3f605e02f3e7196dd13ed62a63
彭道宽 <pengdaokuan@cvte.com>
2021-04-23 15:56:09 +0800

    [feature][what]删除无用的文件夹

--- app/renderer/container/resume/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './index.less';
-
-function Resume() {
-  return <div styleName="root">彭道宽</div>;
-}
-
-export default Resume;
--- app/renderer/container/root/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './index.less';
-
-function Root() {
-  return <div styleName="root">彭道宽</div>;
-}
-export default Root;
