84a438fb6bcc729d99ea84cecaa7e65f0fa17e86
彭道宽 <pengdaokuan@cvte.com>
2021-04-23 15:42:56 +0800

    [feature][what]初始化项目

--- app/renderer/app.tsx
+++ app/renderer/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import MyScrollBox from '@common/components/MyScrollBox';
+
 function App() {
   return (
     <Router>
--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -9,8 +9,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
-      '@src': path.join(__dirname, '../', 'src/'),
-      '@common': path.join(__dirname, '../', 'common/'),
+      '@src': path.join(__dirname, '../', 'app/renderer/'),
+      '@assets': path.join(__dirname, '../', 'assets/'),
     },
   },
   module: {
