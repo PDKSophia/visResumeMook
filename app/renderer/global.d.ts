59a3df56a77d3a5fdf53886620ffb21cf59b942a
彭道宽 <pengdaokuan@cvte.com>
2021-04-24 11:48:14 +0800

    [feature][what]修改tsconfig，添加global.d.ts，处理引入图片文件，TS会报错问题

---
+++ app/renderer/global.d.ts
@@ -0,0 +1,14 @@
+declare module '*.png' {
+  const png: string;
+  export default png;
+}
+
+declare module '*.gif' {
+  const gif: string;
+  export default gif;
+}
+
+declare module '*.svg' {
+  const svg: string;
+  export default svg;
+}
