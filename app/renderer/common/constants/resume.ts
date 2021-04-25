15b615d3bb320831cdab298f84ea90da78e342f6
彭道宽 <pengdaokuan@cvte.com>
2021-04-25 23:30:52 +0800

    [feature][what]修改常量名

--- app/renderer/common/constants/resume.ts
+++ app/renderer/common/constants/resume.ts
@@ -1,76 +1,76 @@
-export const ResumeSliderActionMaps = {
-  Personal: 'personal', // 个人信息
-  Education: 'education', // 教育信息
-  WorkPrefer: 'workPrefer', // 工作期望
-  SchoolExperience: 'schoolExperience', // 在校经历
-  ProjectExperience: 'projectExperience', // 项目经验
-  WorkExperience: 'workExperience', // 工作经历
-  Certificate: 'certificate', // 获奖证书
-  Evaluation: 'evaluation', // 个人评价
-  Skill: 'skill', // 专业技能
-  Test: 'test', // 测试用例
+export const RESUME_SLIDER_MAPS = {
+  personal: 'personal', // 个人信息
+  education: 'education', // 教育信息
+  workPrefer: 'workPrefer', // 工作期望
+  schoolExperience: 'schoolExperience', // 在校经历
+  projectExperience: 'projectExperience', // 项目经验
+  workExperience: 'workExperience', // 工作经历
+  certificate: 'certificate', // 获奖证书
+  evaluation: 'evaluation', // 个人评价
+  skill: 'skill', // 专业技能
+  test: 'test', // 测试用例
 };
 
-const ResumeSliderActionList: TSResume.SliderItem[] = [
+const RESUME_SLIDER_LIST: TSResume.SliderItem[] = [
   {
-    key: ResumeSliderActionMaps.Personal,
+    key: RESUME_SLIDER_MAPS.personal,
     name: '个人信息',
     require: true,
     summary: '更好介绍自己，机会会更多',
   },
   {
-    key: ResumeSliderActionMaps.Education,
+    key: RESUME_SLIDER_MAPS.education,
     name: '教育信息',
     require: false,
     summary: '介绍你的学校和专业信息',
   },
   {
-    key: ResumeSliderActionMaps.WorkPrefer,
+    key: RESUME_SLIDER_MAPS.workPrefer,
     name: '工作期望',
     require: false,
     summary: '聊聊你所期望的宜人办公城市',
   },
   {
-    key: ResumeSliderActionMaps.SchoolExperience,
+    key: RESUME_SLIDER_MAPS.schoolExperience,
     name: '在校经历',
     require: false,
     summary: '展示在校任职成果和人际关系',
   },
   {
-    key: ResumeSliderActionMaps.ProjectExperience,
+    key: RESUME_SLIDER_MAPS.projectExperience,
     name: '项目经验',
     require: false,
     summary: '展示研究过什么优秀项目和成果',
   },
   {
-    key: ResumeSliderActionMaps.WorkExperience,
+    key: RESUME_SLIDER_MAPS.workExperience,
     name: '工作经历',
     require: false,
     summary: '申请岗位的相关经验和能力',
   },
   {
-    key: ResumeSliderActionMaps.Certificate,
+    key: RESUME_SLIDER_MAPS.certificate,
     name: '获奖证书',
     require: false,
     summary: '得过什么奖项值得炫耀',
   },
   {
-    key: ResumeSliderActionMaps.Evaluation,
+    key: RESUME_SLIDER_MAPS.evaluation,
     name: '个人评价',
     require: false,
     summary: '低调夸一夸自己有什么亮点',
   },
   {
-    key: ResumeSliderActionMaps.Skill,
+    key: RESUME_SLIDER_MAPS.skill,
     name: '专业技能',
     require: false,
     summary: '展示具备的技能，突出你的能力',
   },
   {
-    key: ResumeSliderActionMaps.Test,
+    key: RESUME_SLIDER_MAPS.test,
     name: '测试用例',
     require: true,
     summary: '测试用例，一键覆盖',
   },
 ];
-export default ResumeSliderActionList;
+export default RESUME_SLIDER_LIST;
