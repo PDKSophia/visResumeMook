28e7f9d9ac21597b4e29f97919bae2973f5a9b2e
彭道宽 <pengdaokuan@cvte.com>
2021-05-17 20:13:58 +0800

    [feature][wha]内容修改

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// 暂不处理：https://github.com/typescript-eslint/typescript-eslint/blob/v4.22.0/packages/eslint-plugin/docs/rules/no-var-requires.md
 const path = require('path');
 
 module.exports = {
