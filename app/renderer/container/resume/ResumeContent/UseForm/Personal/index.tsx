655f677a5db49ee646b1c1d06dbf8bfd3eaffd84
彭道宽 <pengdaokuan@cvte.com>
2021-05-27 12:46:18 +0800

    [linted][feature][what]完成表单弹窗组件

---
+++ app/renderer/container/resume/ResumeContent/UseForm/Personal/index.tsx
@@ -0,0 +1,63 @@
+/**
+ * @description 个人信息的弹窗表单组件
+ */
+import React from 'react';
+import './index.less';
+import MyModal from '@common/components/MyModal';
+import MyInput from '@common/components/MyInput';
+import { useSelector } from 'react-redux';
+
+function Personal() {
+  const hobby: string = useSelector((state: any) => state.resumeModel.hobby);
+  const base: TSResume.Base = useSelector((state: any) => state.resumeModel.base);
+  return (
+    <MyModal.Dialog
+      title="个人信息"
+      showFooter={false}
+      config={{
+        cancelBtn: {
+          callback: () => {},
+        },
+      }}
+    >
+      <div styleName="form">
+        <div styleName="flex">
+          <div styleName="left">
+            <span styleName="require">*</span>姓 名 ：
+          </div>
+          <div styleName="right">
+            <MyInput onChange={(e) => {}} value={base?.username || ''} placeholder="请输入姓名" allowClear={true} />
+          </div>
+        </div>
+        {/* <div styleName="flex">
+          <div styleName="left">
+            <span styleName="require">*</span>籍 贯 ：
+          </div>
+          <div styleName="right">
+            <MyInput onChange={(e) => {}} value={base?.hometown || ''} placeholder="请输入籍贯" allowClear={true} />
+          </div>
+        </div>
+        <div styleName="flex">
+          <div styleName="left">
+            <span styleName="require" style={{ opacity: 0 }}>
+              *
+            </span>
+            爱 好 ：
+          </div>
+          <div styleName="right">
+            <MyInput
+              type="textarea"
+              onChange={(e) => {}}
+              rows={5}
+              value={hobby || ''}
+              placeholder="你有什么特长爱好呢"
+              allowClear={true}
+            />
+          </div>
+        </div> */}
+      </div>
+    </MyModal.Dialog>
+  );
+}
+
+export default Personal;
--- app/renderer/container/resume/ResumeContent/index.tsx
+++ app/renderer/container/resume/ResumeContent/index.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.less';
 import * as UseTemplateList from './UseTemplate';
 import MyScrollBox from '@common/components/MyScrollBox';
 import Messager, { MESSAGE_EVENT_NAME_MAPS } from '@common/messager';
+import { RESUME_TOOLBAR_MAPS } from '@common/constants/resume';
+import PersonalForm from './UseForm/Personal';
 
 function ResumeContent() {
   const HEADER_ACTION_HEIGHT = 92;
   const height = document.body.clientHeight;
+  const [formName, setFormName] = useState('');
+  const [showFormModal, setShowFormModal] = useState(false);
+
   useEffect(() => {
     document.addEventListener(MESSAGE_EVENT_NAME_MAPS.OPEN_FORM_MODAL, onReceive);
     return () => {
@@ -18,12 +23,19 @@ function ResumeContent() {
    */
   const onReceive = (e: any) => {
     Messager.receive(e, (data: any) => {
-      console.log('发布订阅，传参值为: ', data);
+      setShowFormModal(true);
+      setFormName(data?.form_name);
     });
   };
   return (
     <MyScrollBox maxHeight={height - HEADER_ACTION_HEIGHT}>
       <UseTemplateList.TemplateOne />
+      {showFormModal && (
+        <>
+          {formName === RESUME_TOOLBAR_MAPS.personal && <PersonalForm />}
+          {/* {formName === RESUME_TOOLBAR_MAPS.education && <EducationForm />} */}
+        </>
+      )}
     </MyScrollBox>
   );
 }
