b0d477de9c978c8e0d99f6e5259da58adb199138
彭道宽 <pengdaokuan@cvte.com>
2021-06-25 10:17:31 +0800

    [feature][what]完成模版列表侧边栏的动态列表数据

--- app/renderer/container/templateList/Navigation/index.tsx
+++ app/renderer/container/templateList/Navigation/index.tsx
@@ -1,38 +1,49 @@
+/*
+ * @Description: 模版列表侧边栏
+ * @Author: pengdaokuan
+ * @LastEditors: pengdaokuan
+ * @Date: 2021-06-25 08:56:12
+ * @LastEditTime: 2021-06-25 10:12:37
+ */
 import React from 'react';
 import './index.less';
 import UseIcon from '@assets/icon/use.png';
-import TemplateCoverOne from '@assets/template/template1.jpg';
-import TemplateCoverTwo from '@assets/template/template2.jpg';
 import MyScrollBox from '@common/components/MyScrollBox';
 import MyButton from '@common/components/MyButton';
+import { useSelector } from 'react-redux';
 
 function Navigation() {
+  const HEADER_HEIGHT = 92;
   const height = document.body.clientHeight;
+  const templateList: TSTemplate.Item[] = useSelector((state: any) => state.templateModel.templateList);
+  const selectTemplate: TSTemplate.Item = useSelector((state: any) => state.templateModel.selectTemplate);
 
   return (
     <div styleName="navigation">
-      <MyScrollBox maxHeight={height - 60 - 32}>
-        {/*  */}
-        <div styleName="template">
-          <img styleName="cover" src={TemplateCoverOne} />
-          <div styleName="mask">
-            <img styleName="use" src={UseIcon} />
-          </div>
-        </div>
-        <div styleName="template">
-          <img styleName="cover" src={TemplateCoverTwo} />
-          <div styleName="mask">
-            <MyButton
-              size="middle"
-              className="view-btn"
-              onClick={() => {
-                console.log(1);
-              }}
-            >
-              预览模版
-            </MyButton>
-          </div>
-        </div>
+      <MyScrollBox maxHeight={height - HEADER_HEIGHT}>
+        {templateList &&
+          templateList.length > 0 &&
+          templateList.map((template: TSTemplate.Item) => {
+            return (
+              <div styleName="template" key={template?.templateId}>
+                <img styleName="cover" src={template?.templateCover} />
+                <div styleName="mask">
+                  {selectTemplate?.templateId === template?.templateId && <img styleName="use" src={UseIcon} />}
+                  {selectTemplate?.templateId !== template?.templateId && (
+                    <MyButton
+                      size="middle"
+                      className="view-btn"
+                      onClick={() => {
+                        console.log(1);
+                      }}
+                    >
+                      预览模版
+                    </MyButton>
+                  )}
+                </div>
+              </div>
+            );
+          })}
       </MyScrollBox>
     </div>
   );
