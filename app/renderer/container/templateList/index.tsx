8eddcaf6caa07a2e42f42391e54870673d87b9eb
彭道宽 <pengdaokuan@cvte.com>
2021-06-25 11:06:23 +0800

    [feature][what]引入MyRectSize组件，删除静态效果时的一些样式代码

--- app/renderer/container/templateList/index.tsx
+++ app/renderer/container/templateList/index.tsx
@@ -1,16 +1,30 @@
+/*
+ * @Description:
+ * @Author: pengdaokuan
+ * @LastEditors: pengdaokuan
+ * @Date: 2021-06-25 08:56:12
+ * @LastEditTime: 2021-06-25 10:36:25
+ */
 import React from 'react';
 import './index.less';
 import Header from './Header';
 import Navigation from './Navigation';
 import StaticResume from './StaticResume';
+import MyRectSize from '@common/components/MyRectSize';
 
 function TemplateList() {
   return (
     <div styleName="container">
       <Header />
       <div styleName="content">
-        <Navigation />
-        <StaticResume />
+        <MyRectSize>
+          <MyRectSize.Left>
+            <Navigation />
+          </MyRectSize.Left>
+          <MyRectSize.Right>
+            <StaticResume />
+          </MyRectSize.Right>
+        </MyRectSize>
       </div>
     </div>
   );
