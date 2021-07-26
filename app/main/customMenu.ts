534a6a379ce79214280be7952f1c85afa9c6ed68
彭道宽 <pengdaokuan@cvte.com>
2021-07-26 12:43:29 +0800

    [linted][linted][feature][what]完成自定义原生应用菜单栏

---
+++ app/main/customMenu.ts
@@ -0,0 +1,138 @@
+import _ from 'lodash';
+import { MyBrowserWindow } from './electron';
+import { MenuItemConstructorOptions, shell, MenuItem, BrowserWindow } from 'electron';
+
+const customMenu: (MenuItemConstructorOptions | MenuItem)[] = [
+  {
+    label: '平台',
+    role: 'help',
+    submenu: [
+      {
+        label: '源码',
+        click: function () {
+          shell.openExternal('https://github.com/PDKSophia/visResumeMook');
+        },
+      },
+      {
+        label: '小册',
+        click: function () {
+          shell.openExternal('https://juejin.cn/book/6950646725295996940');
+        },
+      },
+    ],
+  },
+  {
+    label: '编辑',
+    submenu: [
+      {
+        label: '撤销',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo',
+      },
+      {
+        label: '重做',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo',
+      },
+      {
+        type: 'separator',
+      },
+      {
+        label: '剪切',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut',
+      },
+      {
+        label: '复制',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy',
+      },
+      {
+        label: '粘贴',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste',
+      },
+    ],
+  },
+  {
+    label: '窗口',
+    role: 'window',
+    submenu: [
+      {
+        label: '最小化',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize',
+      },
+      {
+        label: '关闭',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close',
+      },
+      {
+        type: 'separator',
+      },
+    ],
+  },
+  {
+    label: '视图',
+    submenu: [
+      {
+        label: '刷新当前页面',
+        accelerator: 'CmdOrCtrl+R',
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.reload();
+          }
+        },
+      },
+      {
+        label: '切换全屏幕',
+        accelerator: (() => {
+          if (process.platform === 'darwin') {
+            return 'Ctrl+Command+F';
+          } else {
+            return 'F11';
+          }
+        })(),
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+          }
+        },
+      },
+      {
+        label: '切换开发者工具',
+        role: 'toggleDevTools',
+        accelerator: (() => {
+          if (process.platform === 'darwin') {
+            return 'Alt+Command+I';
+          } else {
+            return 'Ctrl+Shift+I';
+          }
+        })(),
+        click: (item, focusedWindow) => {
+          if (focusedWindow) {
+            focusedWindow.webContents.openDevTools();
+          }
+        },
+      },
+    ],
+  },
+  {
+    label: '设置',
+    submenu: [
+      {
+        label: '修改简历数据储存路径',
+        click: () => {
+          const wins: MyBrowserWindow[] = BrowserWindow.getAllWindows();
+          const currentWindow = _.find(wins, (w) => w.uid === 'settingWindow');
+          if (currentWindow) {
+            currentWindow.show();
+          }
+        },
+      },
+    ],
+  },
+];
+
+export default customMenu;
